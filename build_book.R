f60a88dae4741fd9117c1dbb457c0b7067368ab3
Jeff Baldock <baldockj@oregonstate.edu>
2026-04-10 11:04:52 -0600

    update cv, pubs, fix links

--- build_book.R
+++ build_book.R
@@ -3,11 +3,6 @@ library(knitr)
 
 # To add a chapter to the book, or shuffle the order of chapters, first render the single chapter, then render the entire book (after updating the .yml file)
 
-# Render all
-# Need to delete caches and files for the following qmd files before rendering
-#  - CollateData
-#  - TemporalResolution
-#  - PASTA
 quarto::quarto_render(output_format = "html")
 
 quarto::quarto_render("cv/cv.qmd", output_format = "html")

93064098c2925abf9b286b20ed2b32c4f00a8ded
Jeff Baldock <baldockj@oregonstate.edu>
2026-06-16 19:48:32 -0600

    fix images, SEO meta tags, remove embed-resources

--- build_book.R
+++ build_book.R
@@ -3,7 +3,7 @@ library(knitr)
 
 # To add a chapter to the book, or shuffle the order of chapters, first render the single chapter, then render the entire book (after updating the .yml file)
 
-quarto::quarto_render(output_format = "html")
+quarto::quarto_render(output_format = "html", cache_refresh = TRUE)
 
 quarto::quarto_render("cv/cv.qmd", output_format = "html")
 quarto::quarto_render("dataprod/dataproducts.qmd", output_format = "html")
